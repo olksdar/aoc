1e0a8f2e3a679e24eaa1be54ec786ef35328a806
Alex Pushkar <opushkar@duck.com>
2023-06-05 17:48:35 +0200

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,17 @@
 // Also good if elevation of destination 
 // square is less then current elevation.
 //
-const INPUT_FOR_VERIFICATION: &str= "Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi";
 
 // Values for mark start and end positions
 const START_VALUE: u8 = 30;
 const END_VALUE: u8 = 40;
 
 struct MapDesc {
-    rows: u16,
-    columns: u16,
+    rows: usize,
+    columns: usize,
     data: Vec<u8>,
+    start: usize,
+    end: usize,
 }
 
 fn read_input(input: &str) -> MapDesc {
@@ -32,36 +29,42 @@ fn read_input(input: &str) -> MapDesc {
     
     let mut data = vec![0; input_len];
 
-    let mut columns: Option<u16> = None;
-    let mut rows: u16 = 0;
+    let mut columns: Option<usize> = None;
+    // At least one row is available
+    let mut rows: usize= 1;
     let mut cnt: usize = 0;
+    let mut start: Option<usize> = None;
+    let mut end: Option<usize> = None;
+
     for x in bytes {
 
         if NEW_LINE_SYMBOL == x {
             println!();
             rows += 1;
-            columns.get_or_insert(cnt.try_into().unwrap());
+            columns.get_or_insert(cnt);
             continue;
         }
 
         data[cnt] = 
         match x {
-            START_SYMBOL_BYTE => START_VALUE,
-            END_SYMBOL_BYTE => END_VALUE,
+            START_SYMBOL_BYTE => {start.get_or_insert(cnt); START_VALUE},
+            END_SYMBOL_BYTE => {end.get_or_insert(cnt); END_VALUE},
             _ => x - BASE_SYMBOL_BYTE };
 
         println!("[{}] {} -> {}", cnt, x, data[cnt]);
         cnt += 1;
     }
 
-    let columns: u16 = columns.unwrap();
+    let columns: usize = columns.unwrap();
     // Remove elements allocated for newlines
-    data.truncate((columns * rows).into());
+    data.truncate(columns * rows);
 
     let result = MapDesc {
         rows        : rows,
         columns     : columns,
         data        : data,
+        start       : start.unwrap(),
+        end         : end.unwrap(),
     };
 
     println!();
@@ -77,6 +80,43 @@ fn read_input(input: &str) -> MapDesc {
     result
 }
 
+fn find_path(input_map: &MapDesc) -> u16 {
+
+    0
+}
+
 fn main() {
-    read_input(INPUT_FOR_VERIFICATION);
+//    let map_presentation = read_input(INPUT_FOR_VERIFICATION);
+//    let steps = get_steps(map_presentation);
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    const INPUT_FOR_VERIFICATION: &str= "Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi";
+
+
+    #[test]
+    fn input() {
+        let map_repr = read_input(INPUT_FOR_VERIFICATION);
+        assert_eq!(map_repr.rows, 5);
+        assert_eq!(map_repr.columns, 8);
+        assert_eq!(map_repr.data.len(), 40);
+        assert_eq!(map_repr.start, 0);
+        assert_eq!(map_repr.end, 21);
+    }
+
+    #[test]
+    fn pathfind() {
+        let map_repr = read_input(INPUT_FOR_VERIFICATION);
+        let path_len = find_path(&map_repr);
+        assert_eq!(path_len, 31);
+    }
+
 }
